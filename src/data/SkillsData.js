8726d0c646ea4146743d414a2b3f3ea32dcb2f60
Elalfy Mohamed <46499967+elalfymohamed@users.noreply.github.com>
2021-07-24 22:05:06 +0200

    Add files via upload
    data

---
+++ src/data/SkillsData.js
@@ -0,0 +1,115 @@
+// React Svg
+import react from "../assets/skills/react.svg"
+// Gatsby.js Svg
+import gatsby from "../assets/skills/gatsbyjs.svg"
+// NextJs Svg
+import nextJs from "../assets/skills/next-js.svg"
+// JavaScript Svg
+import js from "../assets/skills/javascript.svg"
+// Html Svg
+import html from "../assets/skills/html5.svg"
+// Pug Svg
+import pug from "../assets/skills/pugjs.svg"
+// Sass Svg
+import sass from "../assets/skills/sass.svg"
+// Webpack Svg
+import webpack from "../assets/skills/webpack.svg"
+// Gulp Svg
+import gulp from "../assets/skills/gulp.svg"
+//  Css Svg
+import css from "../assets/skills/css.svg"
+// Bootstrap Svg
+import bootstrap from "../assets/skills/bootstrap.svg"
+// Github Svg
+import github from "../assets/skills/github.svg"
+// Redux Svg
+import redux from "../assets/skills/redux.svg"
+// ReactNative Svg
+import reactNative from "../assets/skills/react-native.svg"
+
+export const imagesData = [
+  {
+    id: 1,
+    img: react,
+    name: "React",
+    color: `#6ab8e7`,
+  },
+  {
+    id: 3,
+    img: js,
+    name: "JavaScript",
+    color: `#f7df1e`,
+  },
+  {
+    id: 4,
+    img: html,
+    name: "Html5",
+    color: ` #ff5722`,
+  },
+  {
+    id: 5,
+    img: css,
+    name: "Css3",
+    color: ` #2196f3`,
+  },
+  {
+    id: 6,
+    img: gatsby,
+    name: "GatsbyJs",
+    color: `#663399`,
+  },
+  {
+    id: 7,
+    img: nextJs,
+    name: "NextJs",
+    color: `#161614`,
+  },
+  {
+    id: 8,
+    img: redux,
+    name: "Redux",
+    color: `#663399`,
+  },
+  {
+    id: 2,
+    img: reactNative,
+    name: "ReactNative",
+    color: `#6ab8e7`,
+  },
+  {
+    id: 9,
+    img: sass,
+    name: "Sass",
+    color: `#cf649a`,
+  },
+  {
+    id: 10,
+    img: github,
+    name: "Github",
+    color: `#161614`,
+  },
+  {
+    id: 11,
+    img: pug,
+    name: "Pug",
+    color: `#4f4644`,
+  },
+  {
+    id: 12,
+    img: webpack,
+    name: "Webpack",
+    color: `#1c78c0`,
+  },
+  {
+    id: 13,
+    img: gulp,
+    name: "Gulp",
+    color: `#eb4a4b`,
+  },
+  {
+    id: 14,
+    img: bootstrap,
+    name: "Bootstrap",
+    color: `#563d7c`,
+  },
+]
