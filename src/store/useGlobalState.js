e6e74e4a93d1708a43a5280b86f0caef3e3582e7
Elalfy Mohamed <46499967+elalfymohamed@users.noreply.github.com>
2021-08-01 15:28:38 +0200

    store

---
+++ src/store/useGlobalState.js
@@ -0,0 +1,25 @@
+import { useReducer } from "react"
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_DARK_MODE":
+      localStorage.setItem("isDark", !state.isDark)
+      return {
+        isDark: !state.isDark,
+      }
+
+    default:
+      return state
+  }
+}
+
+const useGlobalState = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    isDark: localStorage.getItem("isDark")
+      ? JSON.parse(localStorage.getItem("isDark"))
+      : false,
+  })
+  return { state, dispatch }
+}
+
+export default useGlobalState
