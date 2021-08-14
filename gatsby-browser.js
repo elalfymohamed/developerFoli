d27936d6e7e22f42e5dd26ed6dfece513bf1afe0
Elalfy Mohamed <46499967+elalfymohamed@users.noreply.github.com>
2021-08-14 04:58:58 +0200

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,19 @@
-import './src/style/_main.scss'
+import React from "react"
 
+import "./src/style/_main.scss"
+
+import GlobalStateProvider from "./src/store/GlobalStateProvider"
 
 export const onInitialClientRender = () => {
   setTimeout(function () {
-    document.getElementById("___loader").style.display = "none"
+    document.getElementById("___loader").style.pointerEvents = "none"
+    document.getElementById("___loader").style.clipPath =
+      "circle(60px at 100% -18%)"
   }, 1000)
 }
+
+export const wrapRootElement = ({ element }) => (
+  <GlobalStateProvider>{element}</GlobalStateProvider>
+)
+
 
